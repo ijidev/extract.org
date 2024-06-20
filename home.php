7b95a0b2a229adbccc4ceadc9578c7da4a12064e
ijidev <iheanyichukwui94@gmail.com>
2024-06-20 15:05:06 +0100

    first commit

---
+++ home.php
@@ -0,0 +1,114 @@
+<div class="hero text-center align-items-center bg-info">
+    <h1>
+        GROWING GREAT <br> BUSINESSES
+    </h1>
+    <h4>
+        Extract Marketing aligns your marketing, <br> sales, and  service through 
+        clear communications <br> and connected technology to drive predictable growth.
+    </h4>
+</div>
+
+<div class="row" style="padding:100px 0;">
+    <div class="col-md-6" style="padding:  20px 0 30px 30px;">
+        <h2>Your Growth Solutions Partner</h2>
+        <h4 class="text-info">
+            ALIGNED GROWTH TEAMS + PREDICTABLE PROGRAMS
+        </h4>
+        <p>
+            Are you tired of wasting money on ineffective marketing tactics that leave you 
+            wondering how to analyze results? 
+            Maybe it's time for a more data-driven approach.
+            Growth marketing is the process of using data gained through marketing campaigns 
+            and experimentation to compound growth over time. It can help you anticipate results 
+            and plan your strategies to improve continuously.
+        </p>
+        <quote>
+            <b>
+            As a growth marketing agency, we believe that growing <br> a business is a marathon, 
+            not a sprint.
+            </b>
+        </quote>
+        <p class="pt-4">
+        That's why we created our Growth Programs – to provide a data-driven,  scalable, and fully 
+        managed team of experts focused on generating right-fit leads for your sales team, while 
+        systematically optimizing every aspect of your business for the customer experience.
+        </p>
+        <a href="https://extract-tl-market.com" class="btn btn-info">Marketing Program</a>
+        <!-- <div class="btn p-4 btn-info">Explore</div> -->
+    </div>
+
+    <div class="col-md-6">
+        <img src="assets/img/partner.png" alt="" style="width:95%;">
+    </div>
+        
+
+</div>
+
+
+<div class="row bg-light pt-4 pb-4">
+    <h1 class="text-center pt-4"> OUR SERVICES </h1>
+    <div class="col-sm-4 text-center p-4">
+        <img src="assets/img/market.webp" alt="" style="width:100px; height:100px; border-radius:100%;">
+        <h2>Marketing Programs</h2>
+        <p>
+            Generate more right-fit traffic and leads while building brand recognition with 
+            consistent inbound content and strategic marketing efforts to reach your target niche.
+        </p>
+    </div>
+
+    <div class="col-sm-4 text-center p-4">
+        <img src="assets/img/sale.webp" alt="" style="width:100px; height:100px; border-radius:100%;">
+        <h2>Sales Enablement</h2>
+        <p>
+            Remove friction and empower your sales team to close more deals faster with efficient processes, 
+            software, videos, collateral, and resources they need to succeed.
+        </p>
+    </div>
+
+    <div class="col-sm-4 text-center p-4">
+        <img src="assets/img/service.webp" alt="" style="width:100px; height:100px; border-radius:100%;">
+        <h2>Service Enablement</h2>
+        <p>
+            Improve your customer experience by empowering your service department to solve tickets faster with 
+            efficient processes, software, videos, and resources.
+        </p>
+    </div>
+
+    <div class="col-sm-4 text-center p-4">
+        <img src="assets/img/brand.webp" alt="" style="width:100px; height:100px; border-radius:100%;">
+        <h2>Branding + Design </h2>
+        <p>
+            A strong brand identity and design systems ensure your brand is identifiable, unique, and consistent. 
+            Feel proud and confident that your brand is positioned for success in your niche. 
+        </p>
+    </div>
+
+    <div class="col-sm-4 text-center p-4">
+        <img src="assets/img/web.webp" alt="" style="width:100px; height:100px; border-radius:100%;">
+        <h2>Website Design + Dev</h2>
+        <p>
+            A website should be the best first impression of your brand and your number one sales resource. 
+            From new builds to complex website redesigns, our team delivers results.
+        </p>
+    </div>
+
+    <div class="col-sm-4 text-center p-4">
+        <img src="assets/img/tech.webp" alt="" style="width:100px; height:100px; border-radius:100%;">
+        <h2>Technology + Automation</h2>
+        <p>
+            Feel confident that your data is efficient, structured, and up-to-date. We will establish, connect, 
+            and automate your processes and systems to maximize ROI on your software.
+        </p>
+    </div>
+
+</div>
+
+<div class="bottom bg-info mt-4 pt-4 p-4 text-center">
+<h1 class="text-white pt-4 mt-4">Ready To Grow On Purpose? </h1>
+<h5 pl-4 pr-4>
+    Stop settling for average results. Get the tools and expert team that 
+    you need to reduce friction and achieve scalable, predictable growth.
+</h5>
+    <a href="https://extract-tl-market.com" class="btn btn-light" style="background:orang;">Get Started</a>
+</div>
+
---
+++ index.php
@@ -0,0 +1,76 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="assets/style/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <title>Extract Transform Load Marketing</title>
+    <script type="text/javascript">
+var _smartsupp = _smartsupp || {};
+_smartsupp.key = '79481796682582291e554821fda8fa147bce4e14';
+window.smartsupp||(function(d) {
+  var s,c,o=smartsupp=function(){ o._.push(arguments)};o._=[];
+  s=d.getElementsByTagName('script')[0];c=d.createElement('script');
+  c.type='text/javascript';c.charset='utf-8';c.async=true;
+  c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
+})(document);
+</script>
+<noscript> Powered by <a href=“https://www.smartsupp.com” target=“_blank”>Smartsupp</a></noscript>
+  
+
+</head>
+
+<body>
+    <!-- Nav tabs -->
+    <nav class="top-bar bg-dark pt-2 pb-2">
+        <!-- <div class="row nowrap justify-content-space-between"> -->
+            <div class="logo">
+                <a href="http://">
+                    <img src="assets/img/etlm-logo.png" width="80px">
+                </a>
+            </div>
+            
+            <ul class="nav nav-tabs" id="navId" role="tablist">
+                 <li class="nav-item">
+                     <a href="#home" class="nav-link active p-1" data-bs-toggle="tab" aria-current="page">Home</a>
+                 </li>
+         
+                 <li class="nav-item" role="presentation">
+                     <a href="https://extract-tl-market.com" class="nav-link p-1">marketing</a>
+                 </li>
+    
+                 <li class="nav-item" role="presentation">
+                     <a href="https://widget-page.smartsupp.com/widget/79481796682582291e554821fda8fa147bce4e14" class="nav-link p-1">Support</a>
+                 </li>
+            </ul>
+        <!-- </div> -->
+    </nav>
+
+    <!-- Tab panes -->
+    <div class="tab-content" id="myTabContent">
+        <div class="tab-pane fade show active" id="home" role="tabpanel">
+            <?php include('home.php') ?>
+        </div>
+        <div class="tab-pane fade show active" id="about-us" role="tabpanel">
+            <?php include('about.php') ?>
+        </div>
+       
+    </div>
+
+    <div class="mt-4 pb-4 text-center">Extract Transform Load Marketing <br> All right Reserved</div>
+
+    <!-- (Optional) - Place this js code after initializing bootstrap.min.js or bootstrap.bundle.min.js -->
+    <script>
+        var triggerEl = document.querySelector("#navId a");
+        bootstrap.Tab.getInstance(triggerEl).show(); // Select tab by name
+    </script>
+
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+    crossorigin="anonymous"></script>
+
+</html>
